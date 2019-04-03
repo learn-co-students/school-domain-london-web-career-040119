f4f1ec0c369fe34c7f8404a1ebfdb4734ee87307
A Hashim <ahashim983@yahoo.com>
2019-04-03 20:04:19 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
 # code here!
+class School
+
+
+  def initialize(school)
+    @school = school
+  end
+
+  def roster
+    return {}
+  end
+
+  def grade=(grade)
+    return roster[grade].values
+  end
+
+  def sort
+    sorted_roster = roster.values.sort
+    return sorted_roster
+  end
+
+end
