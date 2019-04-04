0879fa7617ad96bfc17fc8ec5ec8da4d41d7f9d6
hlbtyne <harriet.ballantyne@gmail.com>
2019-04-04 21:58:55 +0100

    Lab passed and completed

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if roster.key?(grade)
+      @roster[grade] << name
+    else
+      @roster[grade]= []
+      @roster[grade] << name
+    end
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    @roster.each_value(&:sort!)
+  end
+
+end
