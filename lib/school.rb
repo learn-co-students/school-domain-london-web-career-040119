3d415159dfea13966df29d5e81c5edd7e9ce16c4
0118ayush <ayush_gehlot@hotmail.com>
2019-04-04 15:58:37 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+
+  attr_accessor :roster
+  attr_reader :school_name
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade].push(student_name)
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    # @roster[genre].sort
+    new_hash ={}
+    a = @roster.each {|key, val|
+      new_hash[key] = val.sort
+    }
+    new_hash
+  end
+
+end
