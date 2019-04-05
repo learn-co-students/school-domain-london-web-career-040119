4127c6a35e01b1912f3cc78e4c2547b0697fa003
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-04-05 12:17:30 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+
+class School
+
+  attr_accessor :roster
+  attr_reader :school
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each do |grade, names|
+      sorted_roster[grade] = names.sort
+    end
+    sorted_roster
+  end
+
+end
